5dd4252a44a3c4a367328a79664a325079b54520
Vidhusha Jothikumar <vidhushajothikumar@gmail.com>
2026-09-01 18:51:49 +0530

    Add Python data types program

---
+++ Datatype.py
@@ -0,0 +1,18 @@
+# Python Data Types
+
+name = "Ajay"          # String
+age = 20               # Integer
+height = 5.8           # Float
+student = True         # Boolean
+
+print("Name:", name)
+print("Name:", type(name))
+
+print("Age:", age)
+print("Age:", type(age))
+
+print("Height:", height)
+print("Height:", type(height))
+
+print("Student:", student)
+print("Student:", type(student))
