b61a80a41a96c89551f6cc65a740a4bf52ff64db
Vidhusha Jothikumar <vidhushajothikumar@gmail.com>
2026-09-01 19:07:35 +0530

    Add Python Operators Program

---
+++ Operators.py
@@ -0,0 +1,30 @@
+# Python Operators
+
+a = 20
+b = 10
+
+# Arithmetic Operators
+print("Arithmetic Operators")
+print("Addition       :", a + b)
+print("Subtraction    :", a - b)
+print("Multiplication :", a * b)
+print("Division       :", a / b)
+print("Modulus        :", a % b)
+
+# Comparison Operators
+print("\nComparison Operators")
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+
+# Assignment Operator
+print("\nAssignment Operator")
+c = a
+print("c =", c)
+
+# Logical Operators
+print("\nLogical Operators")
+print("a > 10 and b > 5 :", a > 10 and b > 5)
+print("a > 25 or b > 5  :", a > 25 or b > 5)
+print("not(a > b)       :", not(a > b))
